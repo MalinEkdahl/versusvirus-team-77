e51c950c79b24d883d6e7c8b613948d61f9ad421
balthazar <balthazar.de.moncuit@gmail.com>
2020-04-04 17:58:04 +0200

    front: add a log service

---
+++ frontend/footprint-calculator/src/app/log-api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from './../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogApiService {
+
+  constructor(private http: HttpClient) { }
+
+  log(json: Object) {
+    this.http.post(environment.logApi + "/answer", json);
+  }
+}
