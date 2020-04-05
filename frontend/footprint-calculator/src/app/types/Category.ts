a710f6b92b230039253255ed631e7a515b4db7e2
balthazar <balthazar.de.moncuit@gmail.com>
2020-04-05 10:03:23 +0200

    Front: add Mobility type value for form and calculator

---
+++ frontend/footprint-calculator/src/app/types/Category.ts
@@ -0,0 +1,10 @@
+/**
+ * Category of the CO2 emitter
+ */
+export enum Category {
+  BENZIN,
+  DIESEL,
+  TRAIN,
+  FOOD,
+  TRAVELLING
+}
---
+++ frontend/footprint-calculator/src/app/types/MobilityValues.ts
@@ -0,0 +1,43 @@
+export type MobilityCarPerYearBefore = {
+  0: "not using",
+  2500: "0 to 5000km",
+  7500: "5000 - 10’000km",
+	12500: "10’000 - 15’000km",
+	17500: "15’000 - 20’000km",
+	25000: "20’000 - 30’000km",
+	30000: "30’000km +"
+}
+
+export type MobilityCarPerWeekAfter = {
+  0: "Not using",
+  125: "0 - 250km",
+  375: "250 - 500km",
+	625: "500 - 750km",
+	875: "750 - 1000km",
+	1125: "1000 - 1250km",
+  1375: "1250 - 1500km",
+  1750: "1500 - 2000km",
+  2000: "2000km +"
+}
+
+export type MobilityPublicTransportPerWeekBefore = {
+  0: "Not using",
+  100: "0 - 200km",
+	300: "200 - 400km",
+	500: "400 - 600km",
+	700: "600 - 800km",
+	900: "800 - 1000km",
+	1100: "1000 - 1200km",
+	1200: "1200km +",
+}
+
+export type MobilityPublicTransportPerWeekAfter = {
+  0: "Not using",
+  100: "0 - 200km",
+	300: "200 - 400km",
+	500: "400 - 600km",
+	700: "600 - 800km",
+	900: "800 - 1000km",
+	1100: "1000 - 1200km",
+	1200: "1200km +",
+}
