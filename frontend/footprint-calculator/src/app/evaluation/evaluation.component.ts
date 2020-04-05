e7289dfc14705198bae2b8ee7171ae5d28ff7946
balthazar <balthazar.de.moncuit@gmail.com>
2020-04-05 12:12:18 +0200

    update evalutation layout

--- frontend/footprint-calculator/src/app/evaluation/evaluation.component.ts
+++ frontend/footprint-calculator/src/app/evaluation/evaluation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { CarbonCalculatorService } from '../carbon-calculator.service';
 
 @Component({
   selector: 'app-evaluation',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class EvaluationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public carbonCalculator: CarbonCalculatorService) { }
 
   ngOnInit(): void {
   }
