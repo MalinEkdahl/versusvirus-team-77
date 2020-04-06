0c62fb3709806780b9289692c07c6f96ae3e21e7
Malin Ekdahl <malin.ekdahl@gmx.ch>
2020-04-06 10:25:41 +0200

    fix hotel stays

--- frontend/footprint-calculator/src/app/carbon-calculator.service.ts
+++ frontend/footprint-calculator/src/app/carbon-calculator.service.ts
@@ -62,10 +62,10 @@ export class CarbonCalculatorService {
     const lockdown = new Date(2020, 2, 16);
     const today = new Date();
     const daysGoneBy = (today.getTime() - lockdown.getTime()) / (1000 * 3600 * 24);
-    this.userResults.travel.before = (this.userSelection.hoursSpentInAirplane * categoryConst.airplane/2) + (this.userSelection.daysSpentOnCruise * categoryConst.cruise/2) + this.userSelection.daysSpentInHotelBefore * categoryConst.hotel;
+    this.userResults.travel.before = (this.userSelection.hoursSpentInAirplane * categoryConst.airplane/2) + (this.userSelection.daysSpentOnCruise * categoryConst.cruise/2) + (this.userSelection.daysSpentInHotelBefore * categoryConst.hotel * 1000);
     this.userResults.travel.after = 365 * (this.userSelection.daysSpentInHotelAfter * categoryConst.hotel / daysGoneBy);
     this.userResults.travel.before /= 1000; // fix calculation of grams to kilograms
-    this.userResults.travel.after /= 1000; // fix calculation of grams to kilograms
+    this.userResults.travel.after /= 1; // fix calculation not needed because hotels are in kilograms, code left for clarities sake
   }
 
   constructor() { }
